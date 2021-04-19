afeba2ba21db79bb5847891fbf005a6a1b1b3faa
Desere Crawford <desere98@gmail.com>
2021-04-18 19:36:17 -0500

    added the pseudocode and some declarations

--- 4600Project2.cpp
+++ 4600Project2.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <stdio.h>
-
+#include <vector>
 using namespace std;
 
 int main() {
 
-//available: 0, 0, 0; size = num of request
+    int resources = 3//will be changed to the number of resources fromthe matrix
+// Huy and Kwin
+// INPUT:
+// P to R: request
+// R to P: assign/holding
+// top right: request
+// bottom left: assign/allocate
+// only need to read in top right and bottom left of matrix
 
-//while loop till out of process
+
+// store into 2 N-dimension arrays : allocation, request
+// 	i row for P(i)
+// 	j column for R(j)
+// 	(N resource might not match N process)
+
+
+// Desere and Manasa
+// REDUCTION ALGORITHM:
+// available: 0, 0, 0; size = num of request
+    vector(resources, 0);
+
+// while loop till out of process
 // 	check for process that its request <= available
 // 	if none, exit	
 // 	put back that process's holding resource to available 
-
-
-
     return;
 }
