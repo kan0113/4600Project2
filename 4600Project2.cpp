bfdb6d30a9126046ee4a221ecb4b3fa4ec51bcbd
Desere Crawford <desere98@gmail.com>
2021-04-18 19:32:35 -0500

    create file

---
+++ 4600Project2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+//available: 0, 0, 0; size = num of request
+
+//while loop till out of process
+// 	check for process that its request <= available
+// 	if none, exit	
+// 	put back that process's holding resource to available 
+
+
+
+    return;
+}
